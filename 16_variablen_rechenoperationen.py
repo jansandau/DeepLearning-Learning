c58af3c06c87776e766e96148936c74c15974204
Jan <j.sandau@me.com>
2020-10-03 21:05:01 +0200

    Kapitel 14

--- 16_variablen_rechenoperationen.py
+++ 16_variablen_rechenoperationen.py
@@ -6,3 +6,27 @@ alter = 23
 
 print( name + ' ' + str(alter))
 
+#Function with Parameters
+def newUser(name, adresse, alter):
+    newName = name
+    newAdresse = adresse
+    newAlter = alter
+
+    altgenug = False
+
+    #if Statement 
+    if alter > 18: 
+        altgenug = True
+        altersFreigabe(altgenug)
+
+    print("Name: " , newName)
+
+
+def altersFreigabe(checkValue):
+
+    if checkValue == True: 
+        print("Alt genug")
+
+newUser("Jan", "", 23)
+
+print(10 %  2)
