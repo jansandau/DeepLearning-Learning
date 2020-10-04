cbbbeaa333b5ed6c4ee1875c91608156008afdd1
Jan <j.sandau@me.com>
2020-10-04 18:04:51 +0200

    Kapitel 20

---
+++ 20_function.py
@@ -0,0 +1,17 @@
+#functions 
+
+my_list = [-2, 1, -3 , 10 , -100 , -10 , 23]
+
+#function to geht min from the list. list is a parameter
+def getMax(list):
+    result = 0
+    for i in range(1, len(list)):
+        if list[i] < result:
+            result = list[i]
+    
+    print(result)
+
+#call function
+my_list1  = [-23, 1, -3 , 10 , -101 , -10 , 23]
+
+getMax(my_list1)
